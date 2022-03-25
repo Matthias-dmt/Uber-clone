c51c356a940f9bf15bcc076400545992c01b1521
Matthias-dmt <matthiasdumont.pro@gmail.com>
2022-03-25 18:26:15 +0100

    feat: ride option screen

--- components/index.js
+++ components/index.js
@@ -1,1 +1,5 @@
-export { default as NavOptions} from './NavOptions'
+export { default as NavOptions } from "./NavOptions";
+export { default as Map } from "./Map";
+export { default as NavigateCard } from "./NavigateCard";
+export { default as RideOptionsCard } from "./RideOptionsCard";
+export { default as NavFavourites} from "./NavFavourites"
